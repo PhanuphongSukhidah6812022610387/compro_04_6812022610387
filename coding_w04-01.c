d0a34a69040f3bb5ae6824bfbf4ceaeb5ae1c821
PhanuphongSukhidah6812022610387 <s6812022610387@email.kmutnb.ac.th>
2025-07-14 17:03:26 +0700

    งานที่1

--- coding_w04-01.c
+++ coding_w04-01.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() { // keyword
+int main() {   // keyword
     printf("Hello,\nC Tokens!\n");
-    return 0; // keyword
+    return 0;   // keyword
 }
+
+// Keyword: คำสงวนในภาษา C เช่น int, return, main เป็นต้น
+
+// Identifier: ชื่อที่ใช้เรียกตัวแปร ฟังก์ชัน หรือค่าสำคัญ เช่น main
+
+// Constant: ค่าคงที่ เช่น 0, "Hello,", "C Tokens!"
+
+// Operator: เครื่องหมายที่ใช้ในการคำนวณหรือการดำเนินการ เช่น +, -, *, =, , (เครื่องหมายคั่น)
+
